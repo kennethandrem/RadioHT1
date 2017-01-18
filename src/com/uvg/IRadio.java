4bc5c47a31a8b3530f7d4db035149015657d29eb
Kenneth André Martínez Molina <kennethandremm@gmail.com>
2017-01-18 11:41:06 -0600

    Implements IRadio

---
+++ src/com/uvg/IRadio.java
@@ -0,0 +1,15 @@
+package com.uvg;
+
+/**
+ * Created by kennek on 18/01/2017.
+ */
+public interface IRadio {
+
+    boolean status(boolean status);
+
+    boolean change(boolean change);
+
+
+
+
+}

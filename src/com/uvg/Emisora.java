745fc57bd486aa529ebd78e81373dca80cfbaf31
Gadhi Rodriguez <Gadhi Rodriguez>
2017-01-18 11:32:11 -0600

    Modelo y GUI

---
+++ src/com/uvg/Emisora.java
@@ -0,0 +1,29 @@
+package com.uvg;
+
+public class Emisora {
+	private String nombre;
+	private boolean fMaM;
+	private float numEstacion;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public boolean isfMaM() {
+		return fMaM;
+	}
+	public void setfMaM(boolean fMaM) {
+		this.fMaM = fMaM;
+	}
+	public float getNumEstacion() {
+		return numEstacion;
+	}
+	public void setNumEstacion(float numEstacion) {
+		this.numEstacion = numEstacion;
+	}
+	
+}
